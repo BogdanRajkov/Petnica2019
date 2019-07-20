f47a9282e9140064939e09966fa44fd64d47f196
BRuteforce123 <33605382+BogdanRajkov@users.noreply.github.com>
2019-07-20 21:13:48 +0200

    Log file

--- nacrtaj.m
+++ nacrtaj.m
@@ -1,10 +1,18 @@
-addpath('./ravan');
+inputID = fopen('input.txt', 'r');
+name = fgetl(inputID);
+addpath(strcat('./', name));
+temp = fscanf(inputID, ' %d %d;', [2 Inf]);
+fclose(inputID);
+len = size(temp);
+temp = reshape(temp, [2 len(2)/2 2]);
+start_rays = temp(:, :, 1)';
+start_kvector = temp(:, :, 2)';
 
-start_rays = [ones(1, 5) * -5; -4 : 2 : 4]';
-start_kvector = repmat([1 1], 5, 1);
 len = size(start_rays);
 len = len(1);
 setup();
+
+tic;
 
 hold on;
 for it = 1:len
@@ -14,7 +22,7 @@ for it = 1:len
    kvector_arr = zeros(1e6, 2);
    position_arr(1,:)
    t = 1;
-   while abs(position(1)) <= 5
+   while norm(position) > .1
        [position, kvector] = propagate(position, kvector);
        position_arr(t,:) = position;
        kvector_arr(t,:) = kvector;
@@ -23,4 +31,17 @@ for it = 1:len
    plot(position_arr(1:t-1,1), position_arr(1:t-1,2));
 end
 hold off;
+saveas(gcf, strcat(datestr(now, 'yyyy-mm-dd HH:MM'), '.png'));
 
+timeElapsed = toc;
+
+%%
+outputID = fopen(strcat(datestr(now, 'yyyy-mm-dd HH:MM'), '.log'), 'w');
+fprintf(outputID, '---INPUT---\r\n\r\n');
+fprintf(outputID, strcat(name, '\r\n'));
+fprintf(outputID, ' %d %d;', start_rays');
+fprintf(outputID, '\r\n');
+fprintf(outputID, ' %d %d;', start_kvector');
+fprintf(outputID, '\r\n\r\n---OUTPUT---\r\n\r\n');
+fprintf(outputID, strcat('\r\nDuration: ', num2str(timeElapsed), ' s\r\n'));
+fclose(outputID);
