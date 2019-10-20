7d927b9eecab018bc44da021491adda7e64ae2f9
BRuteforce123 <33605382+BogdanRajkov@users.noreply.github.com>
2019-10-20 23:47:55 +0200

    novi rezultati opet

---
+++ 'rezultati'/fminsearch_n/eksponent.py
@@ -0,0 +1,31 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+k_arr = [7, 8, 9, 10, 11]
+exp = {}
+
+for i in k_arr:
+	path = 'fitovi/n'+str(i)+'.log'
+	f = open(path)
+	exp[i] = float(f.readline().split()[-1])
+	f.close()
+	#print(exp[i])
+
+y = [exp[i] for i in k_arr]
+plt.scatter(k_arr, y)
+
+# t = np.linspace(k_arr[0], k_arr[-1])
+# A = np.vstack([k_arr, np.ones(len(k_arr))]).T
+# [[m, c], res] = np.linalg.lstsq(A, y, rcond=None)[:2]
+# plt.plot(t, m*t + c)
+
+plt.suptitle('Zavisnost eksponenta od broja slojeva')
+plt.xlabel('Broj slojeva')
+plt.ylabel('Fitovani eksponent indeksa prelamanja')
+plt.savefig('eksponent.jpg')
+
+# f = open('eksponent.log', 'w')
+# f.write('m = '+str(m)+'\n')
+# f.write('c = '+str(c)+'\n')
+# f.write('res = '+str(res[0])+'\n')
+# f.close()
--- 'rezultati'/fminsearch_n/readme.txt
+++ 'rezultati'/fminsearch_n/readme.txt
@@ -2,3 +2,4 @@ podaci: log fajlovi dobijeni iz simulacije za ekvidistantne slojeve
 slike: grafici logaritma indeksa prelamanja u zavisnosti od rastojanja, ponekad u dve verzije: bez nastavka su fitovane sve tacke, nastavak '_t' znaci da su fitovane samo tacke koje izgledaju linearno
 fitovi: logaritam indeksa se fituje na oblik m*x+c, a res predstavlja zbir kvadrata gresaka, nastavak '_t' se odnosi na 'skraceni' fit
 plot generise slike i fit log fajlove
+eksponent: crta grafik eksponenata koji se dobijaju iz fitova
